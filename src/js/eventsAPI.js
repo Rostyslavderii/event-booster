a015ea24cb0ab05801aa18d56448939b54434f04
Rostyslav Derii <105818935+Rostyslavderii@users.noreply.github.com>
2022-12-06 00:16:43 +0200

    Initialize anya-add-include

---
+++ src/js/eventsAPI.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+export class EventsAPI{
+    static #apikey = "GfgVh4R7D4nbAHFUVOA6O71Si5aOA40O";
+    static #params ={};
+    static #totalPages = 0; 
+    static #currentPage = 0;
+    /**
+     * 
+     * @param {options} options - Object of options of query look into API_DOC
+     * 
+     * @returns - Array of events
+     */
+    static async getEvents(options={}){
+        const {countryCode = "", keyword ="", size =""}=options;
+        if(keyword.trim()||countryCode.trim()){
+            this.#params = {
+                ...options, 
+            }
+        }
+        try{
+            const res = await axios.get("https://app.ticketmaster.com/discovery/v2/events.json",
+            {
+                params: {
+                    ...this.#params,
+                    apikey: this.#apikey,
+                }
+            })
+
+            await ({ totalPages: this.#totalPages, number: this.#currentPage } = res.data.page)
+            return res.data._embedded;
+        }catch(e){
+            console.log(e);
+        } 
+    }
+    /**
+     * 
+     * @returns - Current page of Query. Start from ZERO
+     */
+    static getCurrentPage(){
+        return this.#currentPage;
+    }
+    /**
+     * 
+     * @returns - Total pages of events
+     */
+    static getTotalPages(){
+        return this.#totalPages;
+    } 
+}
