f3bdce685de81458d5a8b0f49c77d0a5acefec9a
Rostyslav Derii <105818935+Rostyslavderii@users.noreply.github.com>
2022-12-06 00:16:44 +0200

    Initialize anya-close-and-open-modals

---
+++ src/js/close-and-open-modals.js
@@ -0,0 +1,48 @@
+const openModalTeam = document.querySelector('.js-open-team-modal');
+const modalTeam = document.querySelector('[data-modal-team]');
+const closeTeam = document.querySelector('[data-close-team]');
+const backdropModalTeam = document.querySelector('[data-modal-team]')
+const developers = document.querySelector('.developers')
+
+const modal = document.querySelector('[data-modal]');
+const closeModal = document.querySelector('[data-close]');
+const galleryCard = document.querySelector('.js-gallery-card')
+const backdropModal = document.querySelector('[data-modal]');
+const modalWindow = document.querySelector('.modal')
+
+
+openModalTeam.addEventListener('click', addHiddenClassToTeam);
+closeTeam.addEventListener('click', addHiddenClassToTeam);
+backdropModalTeam.addEventListener('click', (e) => {
+	if (!e.composedPath().includes(developers)) {
+		modalTeam.classList.toggle("visually-hidden");
+	}
+});
+
+galleryCard.addEventListener('click', addHiddenClassToModal);
+closeModal.addEventListener('click', addHiddenClassToModal);
+backdropModal.addEventListener('click', (e) => {
+	if (!e.composedPath().includes(modalWindow)) {
+		modal.classList.toggle("visually-hidden");
+	}
+})
+
+function addHiddenClassToTeam(e) {
+	e.stopPropagation();
+	modalTeam.classList.toggle("visually-hidden");
+	closeByKeybord(modalTeam);
+}
+
+function addHiddenClassToModal(e) {
+	e.preventDefault();
+	modal.classList.toggle("visually-hidden");
+	closeByKeybord(modal)
+}
+
+function closeByKeybord(value) {
+	document.addEventListener("keydown", (e) => {
+		if (e.code == "Escape") {
+			value.classList.toggle("visually-hidden");
+		}
+	}, { once: true });
+}
